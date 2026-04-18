06265dd44eba9759ea8e211d8a3538e8d70c5f5e
11bulba <slotcheri@gmail.com>
2026-04-18 16:56:39 +0300

    Lab 1: Moving Platforms Challenge

--- Assets/Scripts/Components/Movement/VerticalMover.cs
+++ Assets/Scripts/Components/Movement/VerticalMover.cs
@@ -1,16 +1,21 @@
 using UnityEngine;
 
-/// <summary>
-/// Moves transform vertically based on keyboard input (W/S or Arrow keys).
-/// </summary>
 public class VerticalMover : MonoBehaviour
 {
     [Header("Movement Settings")]
-    [SerializeField] private float speed = 5f;
-    
+    [SerializeField] private float speed = 2f;
+    [SerializeField] private float distance = 3f;
+
+    private Vector3 _startPos;
+
+    private void Start()
+    {
+        _startPos = transform.position;
+    }
+
     private void Update()
     {
-        float vertical = Input.GetAxis("Vertical");
-        transform.Translate(Vector3.up * vertical * speed * Time.deltaTime);
+        float newY = Mathf.PingPong(Time.time * speed, distance);
+        transform.position = _startPos + new Vector3(0, newY, 0);
     }
 }
